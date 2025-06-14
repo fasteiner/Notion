cdda87d6b5843c8540b3d35f9ad6bc3d41e57061
Fabian Franz Steiner <75947402+fasteiner@users.noreply.github.com>
2025-06-14 22:35:37 +0000

    Pester Tests for Block Classes #35: Refactor to_do_structure and notion_to_do_block constructors for improved parameter handling and color parsing; add comprehensive Pester tests for constructor and conversion functionality.

--- source/Classes/Block/31_To_do.ps1
+++ source/Classes/Block/31_To_do.ps1
@@ -10,17 +10,18 @@ class to_do_structure
     {
     }
 
-    to_do_structure([rich_text[]] $rich_text, [bool] $checked = $false, [notion_color] $color = "default")
+
+    to_do_structure($rich_text, [bool] $checked = $false, $color = "default")
     {
-        $this.rich_text = $rich_text.rich_text.ForEach({ [rich_text]::ConvertFromObject($_) })
+        $this.rich_text = [rich_text]::ConvertFromObjects($rich_text)
         $this.checked = $checked
-        $this.color = $color
+        $this.color = [Enum]::Parse([notion_color], $color)
     }
 
     static [to_do_structure] ConvertFromObject ($Value)
     {
         $notion_to_do_obj = [to_do_structure]::new()
-        $notion_to_do_obj.rich_text = $Value.rich_text.ForEach({ [rich_text]::ConvertFromObject($_) })
+        $notion_to_do_obj.rich_text = [rich_text]::ConvertFromObjects($Value.rich_text)
         $notion_to_do_obj.checked = $Value.checked
         $notion_to_do_obj.color = [Enum]::Parse([notion_color], $Value.color ?? "default")
         return $notion_to_do_obj
@@ -36,7 +37,12 @@ class notion_to_do_block : notion_block
     {
     }
 
-    notion_to_do_block([rich_text[]] $rich_text, [bool] $checked = $false, [notion_color] $color = "default")
+    notion_to_do_block($rich_text)
+    {
+        $this.to_do = [to_do_structure]::new($rich_text, $false, "default")
+    }
+
+    notion_to_do_block($rich_text, [bool] $checked = $false, $color = "default")
     {
         $this.to_do = [to_do_structure]::new($rich_text, $checked, $color)
     }
--- tests/Unit/Classes/Block/31_To_do.tests.ps1
+++ tests/Unit/Classes/Block/31_To_do.tests.ps1
@@ -1,1 +1,80 @@
+Import-Module Pester
 
+BeforeDiscovery {
+    $projectPath = "$($PSScriptRoot)/../../../.." | Convert-Path
+
+    if (-not $ProjectName)
+    {
+        $ProjectName = Get-SamplerProjectName -BuildRoot $projectPath
+    }
+    Write-Debug "ProjectName: $ProjectName"
+    $global:moduleName = $ProjectName
+
+    Remove-Module -Name $global:moduleName -Force -ErrorAction SilentlyContinue
+    $mut = Import-Module -Name "$projectPath/output/module/$ProjectName" -Force -ErrorAction Stop -PassThru
+}
+
+Describe "notion_to_do_block Tests" {
+    Context "Constructor Tests" {
+        It "Should create an empty notion_to_do_block" {
+            $block = [notion_to_do_block]::new()
+            $block | Should -BeOfType "notion_to_do_block"
+            $block.type | Should -Be "to_do"
+            $block.to_do | Should -BeNullOrEmpty
+        }
+
+        It "Should create a notion_to_do_block with rich_text only" {
+            $rt = [rich_text_text]::new("Task 1")
+            $block = [notion_to_do_block]::new(@($rt))
+            $block.to_do | Should -Not -BeNullOrEmpty
+            ($block.to_do.GetType().Name) | Should -Be "to_do_structure"
+            $block.to_do.rich_text[0].plain_text | Should -Be "Task 1"
+            $block.to_do.checked | Should -BeFalse
+            $block.to_do.color.ToString() | Should -Be "default"
+        }
+
+        It "Should create a notion_to_do_block with all parameters" {
+            $rt = [rich_text_text]::new("Task 2")
+            $block = [notion_to_do_block]::new(@($rt), $true, "green")
+            $block.to_do.checked | Should -BeTrue
+            $block.to_do.color.ToString() | Should -Be "green"
+        }
+    }
+
+    Context "ConvertFromObject Tests" {
+        It "Should convert from object correctly" {
+            $mock = [PSCustomObject]@{
+                to_do = [PSCustomObject]@{
+                    rich_text = @([PSCustomObject]@{
+                        type = "text"
+                        text = @{ content = "Converted task" }
+                        plain_text = "Converted task"
+                    })
+                    checked = $true
+                    color = "blue"
+                }
+            }
+            $block = [notion_to_do_block]::ConvertFromObject($mock)
+            $block | Should -BeOfType "notion_to_do_block"
+            ($block.to_do.GetType().Name) | Should -Be "to_do_structure"
+            $block.to_do.rich_text[0].plain_text | Should -Be "Converted task"
+            $block.to_do.checked | Should -BeTrue
+            $block.to_do.color.ToString() | Should -Be "blue"
+        }
+
+        It "Should default to 'default' color if not provided" {
+            $mock = [PSCustomObject]@{
+                to_do = [PSCustomObject]@{
+                    rich_text = @([PSCustomObject]@{
+                        type = "text"
+                        text = @{ content = "No color task" }
+                        plain_text = "No color task"
+                    })
+                    checked = $false
+                }
+            }
+            $block = [notion_to_do_block]::ConvertFromObject($mock)
+            $block.to_do.color.ToString() | Should -Be "default"
+        }
+    }
+}
